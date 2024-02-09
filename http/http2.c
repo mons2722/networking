9b17ee64a9c9a335621ba9ad68253a435d96cdbf
mons2722 <mons2722@gmail.com>
2024-02-09 19:49:11 +0530

    message transfer

--- http/http2.c
+++ http/http2.c
@@ -8,60 +8,96 @@
 
 struct lws *clients[MAX_CLIENTS];
 
+static int callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len);
+static int callback_websockets(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len);
+
+static struct lws_protocols protocols[] = {
+    {
+        "http-only",
+        callback_http,
+        0,
+    },
+    {
+        "websockets",
+        callback_websockets,
+        0,
+    },
+    { NULL, NULL, 0, 0 }
+};
+
 void forward(struct lws *to, char *mes, size_t len) {
     printf("\n@@\n");
     lws_write(to, (unsigned char *)mes, len, LWS_WRITE_TEXT);
 }
 
-void websocket_handshake(struct lws *wsi, char *request, size_t len) {
-    char response[] = "HTTP/1.1 101 Switching Protocols\r\n"
-                      "Upgrade: websocket\r\n"
-                      "Connection: Upgrade\r\n"
-                      "\r\n";
-    lws_write(wsi, (unsigned char *)response, strlen(response), LWS_WRITE_HTTP);
-}
+int main() {
+    struct lws_context_creation_info info;
+    struct lws_context *context;
+    int port = PORT;
+    const char *iface = NULL;
+    int opts = 0;
 
-void postreq(struct lws *wsi, char *p, size_t len) {
-    printf("\nReceived Message:\n");
-    int n = len, count = 1;
-    printf("%d %d %d\n", (int)wsi, (int)clients[0], (int)clients[1]);
-    if (wsi == clients[0])
-        forward(clients[1], p, len);
-    else
-        forward(clients[0], p, len);
-    while ((count++) <= n) {
-        if (*p == '+') {
-            putchar(' ');
-            p++;
-            continue;
-        }
-        if (*p == '%') {
-            putchar('\n');
-            p += 6;
-            count += 5;
-            continue;
-        }
-        putchar(*p++);
+    memset(&info, 0, sizeof info);
+    info.port = port;
+    info.iface = iface;
+    info.protocols = protocols;
+    info.ssl_cert_filepath = NULL;
+    info.ssl_private_key_filepath = NULL;
+    info.extensions = NULL;
+    info.gid = -1;
+    info.uid = -1;
+    info.options = opts;
+
+    context = lws_create_context(&info);
+    if (!context) {
+        fprintf(stderr, "Failed to create libwebsockets context\n");
+        return 1;
     }
-    printf("\n");
+
+    printf("Server started. Waiting for connections on port %d...\n", PORT);
+
+    while (1) {
+        lws_service(context, 50);
+    }
+
+    lws_context_destroy(context);
+
+    return 0;
 }
 
-static int callback_http(struct lws *wsi, enum lws_callback_reasons reason,
-                         void *user, void *in, size_t len)
-{
+static int callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
     switch (reason) {
-        case LWS_CALLBACK_HTTP:
-            handle_http_req(wsi, (char *)in, len);
+        case LWS_CALLBACK_HTTP: {
+            char *requested_uri = (char *)in;
+            if (strncmp(requested_uri, "/get", 4) == 0) {
+                // Handle GET request
+                char *response = "HTTP/1.1 200 OK\r\n"
+                                 "Content-Type: text/html\r\n\r\n"
+                                 "<h1>Hello, this is a GET request!</h1>";
+                lws_write(wsi, (unsigned char *)response, strlen(response), LWS_WRITE_HTTP);
+            } else if (strncmp(requested_uri, "/post", 5) == 0) {
+                // Handle POST request
+                char *response = "HTTP/1.1 200 OK\r\n"
+                                 "Content-Type: text/html\r\n\r\n"
+                                 "<h1>Hello, this is a POST request!</h1>";
+                lws_write(wsi, (unsigned char *)response, strlen(response), LWS_WRITE_HTTP);
+            } else {
+                // Error handling for unknown requests
+                char *response = "HTTP/1.1 404 Not Found\r\n"
+                                 "Content-Type: text/html\r\n\r\n"
+                                 "<h1>404 Not Found</h1>"
+                                 "<p>The requested URL was not found on this server.</p>";
+                lws_write(wsi, (unsigned char *)response, strlen(response), LWS_WRITE_HTTP);
+            }
             break;
+        }
         default:
             break;
     }
     return 0;
 }
 
-static int callback_websockets(struct lws *wsi, enum lws_callback_reasons reason,
-                               void *user, void *in, size_t len)
-{
+static int callback_websockets(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
     switch (reason) {
         case LWS_CALLBACK_ESTABLISHED:
             printf("WebSocket client connected\n");
@@ -73,7 +109,14 @@ static int callback_websockets(struct lws *wsi, enum lws_callback_reasons reason
             }
             break;
         case LWS_CALLBACK_RECEIVE:
-            websocket_data(wsi, (char *)in, len);
+            printf("Received message from client\n");
+            // Forward the received message to the other client
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (clients[i] != wsi && clients[i] != NULL) {
+                    forward(clients[i], (char *)in, len);
+                    break; // Forward to only one client
+                }
+            }
             break;
         case LWS_CALLBACK_CLOSED:
             printf("WebSocket client disconnected\n");
@@ -89,37 +132,3 @@ static int callback_websockets(struct lws *wsi, enum lws_callback_reasons reason
     }
     return 0;
 }
-
-static struct lws_protocols protocols[] = {
-    {"http-only", callback_http, 0, 0},
-    {"websocket", callback_websockets, 0, 0},
-    {NULL, NULL, 0, 0}
-};
-
-int main() {
-    struct lws_context_creation_info info;
-    struct lws_context *context;
-
-    memset(&info, 0, sizeof(info));
-    info.port = PORT;
-    info.protocols = protocols;
-    info.gid = -1;
-    info.uid = -1;
-
-    context = lws_create_context(&info);
-    if (!context) {
-        printf("Failed to create libwebsockets context\n");
-        return -1;
-    }
-
-    printf("Server started. Waiting for connections on port %d...\n", PORT);
-
-    while (1) {
-        lws_service(context, 50);
-    }
-
-    lws_context_destroy(context);
-
-    return 0;
-}
-
